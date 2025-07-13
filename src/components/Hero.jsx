477337f02d8db0c7fbfbb42cd29ac9ac8d6c32cc
Ratna Lama <royalray7@gmail.com>
2025-07-12 22:02:20 -0700

    add title and leaf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">OG Bar</h1>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+      </section>
+    </>
+  );
 };
 
 export default Hero;
