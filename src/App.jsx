940896ac6e11c288cbada2d6e1cf1a7c445ad168
Ratna Lama <royalray7@gmail.com>
2025-07-12 20:44:34 -0700

    create utilities

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div className="text-3xl text-indigo-300">Hello World!</div>;
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello World!</h1>
+    </div>
+  );
 };
 
 export default App;
