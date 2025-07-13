071d445354ac2645aa115a9ad61903f71b440dd8
Ratna Lama <royalray7@gmail.com>
2025-07-12 21:15:50 -0700

    add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { navLinks } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <p>Vlevet Pour</p>
+        </a>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`} className="flex items-center gap-2">
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
