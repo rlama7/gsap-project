9cb7e0f7fce4b979010f91853d88ec6e6a2d2019
Ratna Lama <royalray7@gmail.com>
2025-07-15 16:05:52 -0700

    add hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
 
 const Hero = () => {
+  useGSAP((timeline) => {
+    const heroSplit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((chars) => chars.classList.add("text-gradient"));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+  }, []);
   return (
     <>
       <section id="hero" className="noisy">
