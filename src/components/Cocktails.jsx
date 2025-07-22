6f1f6e59550fc03063165c2736a6fbd3bc6cd9c8
Ratna Lama <royalray7@gmail.com>
2025-07-22 15:47:13 -0700

    add cocktails

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import { cocktailLists } from '../../constants';
 
 const Cocktails = () => {
   return (
@@ -13,6 +14,25 @@ const Cocktails = () => {
         alt="right leaf"
         id="c-right-leaf"
       />
+
+      <div className="list">
+        <div className="popular">
+          <h2>Most popular cocktails:</h2>
+          <ul>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="md:me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}{' '}
+                  </p>
+                </div>
+                <span>{price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
